c9da246d78cca11a5b1e2022d4210ecb8fd20c69
YonYoon <zhzhzh1806@gmail.com>
2023-09-24 16:41:16 +0500

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Zhansen Zhalel on 24.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
